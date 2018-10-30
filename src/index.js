91b0bc21a36e47a83283c8193a2725bef750f49e
Mark Greenslade <mark@trinkler.software>
2018-10-30 17:53:38 +0100

    1. Version bump.

--- src/index.js
+++ src/index.js
@@ -8,7 +8,7 @@
  * @fileOverview An image wallet - easier than brain wallets.
  *
  * @exports decode/deriveKey/encode/name/provider/version
- * @version 0.3.2
+ * @version 0.3.3
  */
 
 // Module imports.
@@ -183,7 +183,7 @@ const name = 'Image Wallet';
 const provider = 'Trinkler Software AG';
 
 // Library version.
-const version = '0.3.2';
+const version = '0.3.3';
 
 // Module exports.
 export {
