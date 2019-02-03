ad7f371c9753701d3feddd9575336dbcbbba72da
retotrinkler <reto@trinkler.software>
2019-02-03 10:23:07 +0100

    Add ripple

---
+++ src/coins/xrp.js
@@ -0,0 +1,24 @@
+// Copyright 2018 Trinkler Software AG (Switzerland).
+// Trinkler Software provides free software: you can redistribute it and/or modify
+// it under the terms of the GNU General Public License as published by
+// the Free Software Foundation, either version 3 of the License, or
+// (at your option) any later version <http://www.gnu.org/licenses/>.
+
+/**
+ * @fileOverview Coin metadata plus related functions.
+ */
+
+// SLIP 0044 coin index.
+export const index = 144;
+
+// SLIP 0044 coin hex code.
+export const hexCode = 0x80000090;
+
+// Coin name.
+export const name = 'Ripple';
+
+// Coin ticker symbol.
+export const symbol = 'XRP';
+
+// Elliptic curve name.
+export const curve = 'secp256k1';
