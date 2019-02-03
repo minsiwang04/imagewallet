fb92d31f8ffc8786aca4d738a8bfd2f4fe95c070
retotrinkler <reto@trinkler.software>
2019-02-03 10:13:37 +0100

    Add new cointypes

---
+++ src/coins/bsv.js
@@ -0,0 +1,24 @@
+// Copyright 2018 Trinkler Software AG (Switzerland).
+// Trinkler Software provides free software: you can redistribute it and/or modify
+// it under the terms of the GNU General Public License as published by
+// the Free Software Foundation, either version 3 of the License, or
+// (at your option) any later version <http://www.gnu.org/licenses/>.
+
+/**
+ * @fileOverview Coin metadata plus related functions.
+ */
+
+// SLIP 0044 coin index.
+export const index = 236;
+
+// SLIP 0044 coin hex code.
+export const hexCode = 0x800000ec;
+
+// Coin name.
+export const name = 'Bitcoin SV';
+
+// Coin ticker symbol.
+export const symbol = 'BSV';
+
+// Elliptic curve name.
+export const curve = 'secp256k1';
